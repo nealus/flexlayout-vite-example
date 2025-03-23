a22cb4d7e5ed2bb58f289dbd07953e40f71d1ab0
nmd <ndanger61@gmail.com>
2025-03-23 21:15:28 +0000

    update to v0.8.8 and add + button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import {Layout, Model, TabNode, IJsonModel} from 'flexlayout-react';
+import { Layout, Model, TabNode, IJsonModel, TabSetNode, BorderNode, ITabSetRenderValues, Actions, DockLocation } from 'flexlayout-react';
 import './App.css';
 import 'flexlayout-react/style/light.css';
+import { useRef } from 'react';
 
-var json : IJsonModel= {
-    global: {"tabEnablePopout": true},
-    borders: [],
+var json: IJsonModel = {
+    global: { "tabEnablePopout": true },
+    borders: [
+        {
+            "type": "border",
+            "location": "bottom",
+            "children": [
+                {
+                    "type": "tab",
+                    "name": "JSON",
+                    "component": "json",
+                    "enableClose": false,
+                },
+            ]
+        },
+    ],
     layout: {
         type: "row",
         weight: 100,
@@ -39,18 +53,48 @@ const model = Model.fromJson(json);
 
 function App() {
 
-  const factory = (node: TabNode) => {
-    var component = node.getComponent();
-    if (component === "placeholder") {
-      return <div className="placeholder">{node.getName()}</div>;
-    }
-  }
+    const nextAddIndex = useRef<number>(1);
 
-  return (
-    <Layout
-      model={model}
-      factory={factory} />
-  );
+    const factory = (node: TabNode) => {
+        var component = node.getComponent();
+        switch (component) {
+            case "placeholder":
+                return <div className="placeholder">{node.getName()}</div>;
+                break;
+            case "json":
+                return <pre>{JSON.stringify(json, null, "\t")}</pre>;
+                break;
+            default:
+                return <div>{"unknown component " + component}</div>
+        }
+    }
+
+    const onRenderTabSet = (node: TabSetNode | BorderNode, renderValues: ITabSetRenderValues) => {
+        if (node instanceof TabSetNode) {
+            renderValues.stickyButtons.push(
+                <button
+                    key="Add"
+                    title="Add"
+                    style={{ fontSize: "20px", color: "gray", width: "1em" }}
+                    className="flexlayout__tab_toolbar_button"
+                    onClick={() => {
+                        model.doAction(Actions.addNode({
+                            component: "placeholder",
+                            name: "Added " + nextAddIndex.current++
+                        }, node.getId(), DockLocation.CENTER, -1, true));
+                    }}
+                >+</button>);
+        }
+    }
+
+    return (
+        <Layout
+            model={model}
+            factory={factory}
+            onRenderTabSet={onRenderTabSet}
+            // realtimeResize={true}
+        />
+    );
 }
 
 export default App;
